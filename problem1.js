b7bbeb18b7011937079b7ed4ec7c6d4874c62b2b
Saquib <106192257+Saquib1996@users.noreply.github.com>
2022-06-26 14:07:24 +0000

    Initial commit

---
+++ problem1.js
@@ -0,0 +1,18 @@
+// var x = "10";
+// var y =+(x);
+// console.log(y);
+// var name="      masai       ";
+// console.log(name.trim());
+// var name="    masai    school     ";
+// console.log(name.trim());
+// const str="the quick brown,fox jumps over the lazy dog";
+// var res=str.split("");
+// console.log(res);
+var x=["1","2","3","6"];
+// var res=[];
+// for(var i=0;i<x.length;i++){
+//   res.push(Number(x[i]));
+// }
+// console.log(res);
+var res = x.map(Number)
+console.log(res);
